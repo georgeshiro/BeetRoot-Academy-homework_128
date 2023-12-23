3222499f9b4ac3d451f31f6afcd89b4200107b9c
georgeshiro <ggektarggektar@gmail.com>
2023-12-23 13:22:29 +0200

    super add

---
+++ Exam BHROMAON/js/burgerMenu.js
@@ -0,0 +1,39 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+/**змінює клас елементу з ідентифікатором "myTopnav". Якщо у цього елемента вже є клас "nav", то вона додає до нього ще й клас "responsive". Якщо у цього елемента вже є клас "responsive", то вона замінює його на клас "nav", знову сховуючи бургер-меню. появляется папка дистрикт как резерв
+ * Це функція JavaScript, яка використовується для створення бургер-меню на веб-сторінці. Давайте розберемо кожен рядок коду:
+
+```javascript
+function myFunction() {
+```
+Цей рядок починає визначення функції з іменем `myFunction`.
+```javascript
+var x = document.getElementById("myTopnav");
+```
+Цей рядок отримує елемент з ідентифікатором `"myTopnav"` з документу і зберігає його в змінній `x`.
+```javascript
+if (x.className === "nav") {
+```
+Цей рядок перевіряє, чи містить елемент змінну `className` зі значенням `"nav"`.
+
+```javascript
+x.className += " responsive";
+```
+
+Якщо значення `className` дорівнює `"nav"`, то цей рядок додає до змінної `className` рядок `" responsive"`.
+
+```javascript
+} else {
+  x.className = "nav";
+}
+```
+Якщо значення `className` не дорівнює `"nav"`, то цей рядок встановлює значення `className` на `"nav"`.
+Отже, ця функція використовується для додавання класу `"responsive"` до елемента з ідентифікатором `"myTopnav"`, якщо він має клас `"nav"`. 
+Це змінює вигляд меню на бургер-меню на маленьких екранах.**/
+
